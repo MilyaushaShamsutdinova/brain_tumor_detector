b7b996241c32e2e580d42aba1132fb34e3480d44
MilyaushaShamsutdinova <m.shamsutdinova@innopolis.university>
2024-09-18 14:05:52 +0300

    upd api: del temp img

--- code/deployment/api/api.py
+++ code/deployment/api/api.py
@@ -1,6 +1,4 @@
-import sys
 import os
-
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 from models.model import BrainTumorDetector
@@ -15,17 +13,24 @@ model_path = r'M:\python_projects\brain_tumor_detector\models\model_0.pt'
 data_path = r'M:\python_projects\brain_tumor_detector\code\datasets\Brain-tumor-Detection-1\data.yaml'
 detector = BrainTumorDetector(model_path, data_path)
 
+
+def delete_image(image_path):
+    if os.path.exists(image_path):
+        os.remove(image_path)
+    else:
+        print(f"File not found: {image_path}")
+
+
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     contents = await file.read()
     image = Image.open(io.BytesIO(contents))
     temp_dir = r'M:\python_projects\brain_tumor_detector\data\temp_image.jpg'
-    image.save(temp_dir)  # Saving image temporarily
 
-    # Perform detection
+    image.save(temp_dir)
     result = detector.detect(temp_dir)
+    delete_image(temp_dir)
     
-    # Return result
     return JSONResponse(content={"prediction": result})
 
 if __name__ == "__main__":
