c8620965e8bb0d0966b3fc8899d677718f47aa64
MilyaushaShamsutdinova <m.shamsutdinova@innopolis.university>
2024-09-19 12:42:06 +0300

    model refined

--- code/models/model.py
+++ code/models/model.py
@@ -1,9 +1,5 @@
 from ultralytics import YOLO
-import os
 import yaml
-from typing import List
-import torch
-import time
 
 
 class BrainTumorDetector:
@@ -23,24 +19,8 @@ class BrainTumorDetector:
                 detected_items.add(result.names[box.cls.item()])
         return 'Tumor' if list(detected_items) else 'No tumor'
 
-    def detect(self, image_path) -> List[str]:
+    def detect(self, image_path):
         results = self.model(image_path, conf=self.conf, data=self.data)
         detected_items = self.get_list_of_detected_items(results)
         return detected_items
-
-
-def main():
-    model_path = 'models\model_0.pt'
-    data_path = 'code\datasets\Brain-tumor-Detection-1\data.yaml'
-    conf = 0.5
-    
-    # image_path = r'code\datasets\Brain-tumor-Detection-1\test\images\3_jpg.rf.24e34e13cf2b7e3a657c2a1d04baf721.jpg'
-    image_path = r'data\brain.jpg'
-
-    model = BrainTumorDetector(model_path, data_path)
-    result = model.detect(image_path)
-    print(result)
-
-if __name__ == "__main__":
-    main()
     
